2b3e8eab00eee07eb11a9447ff55c9787f1ee4ad
annclko <anneclaire.koch@pinmeto.com>
2025-11-05 10:47:12 +0100

    remove unecessary endpoint

--- server/server.js
+++ server/server.js
@@ -13,31 +13,4 @@ app.get("/menu", (req, res) => {
   res.json(JSON.parse(data));
 });
 
-app.get("/store-locators", (req, res) => {
-  // Serve HTML page with external script
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Store Locators</title>
-</head>
-<body>
-  <div id="store-locator-container"></div>
-
-  <!-- External script link -->
-  <script src="YOUR_EXTERNAL_SCRIPT_URL_HERE"></script>
-
-  <!-- Optional: Initialize script after it loads -->
-  <script>
-    // Add initialization code here if needed
-    // This will run after the external script loads
-  </script>
-</body>
-</html>
-  `;
-  res.send(html);
-});
-
 app.listen(4000, () => console.log("http://localhost:4000"));
